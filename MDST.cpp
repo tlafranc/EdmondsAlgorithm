6ae93ceff48b9845008d029342941cc8548a7b87
Thomas Lafrance <tlafranc@ualberta.ca>
2020-04-19 13:12:20 -0600

    Added comments and removed iterating over test cases in MDST.cpp, updated README to be more descriptive.

--- MDST.cpp
+++ MDST.cpp
@@ -53,67 +53,62 @@ void DFS(int n, unordered_set<int>& seen);
 vector<Node*> nodes;
 // NOTE: In this program we assume that node 0 is the root node
 int main() {
-    int T;
-    cin >> T;
+    int n, m;
+    cin >> n;
+    cin >> m;
 
-    // Iterate over every test case
-    for (int t = 0; t < T; t++) {
-        int n, m;
-        cin >> n;
-        cin >> m;
+    // Initialize empty nodes
+    nodes.resize(n);
 
-        // Initialize empty nodes
-        nodes.resize(n);
+    for (int i = 0; i < n; i++) {
+        nodes[i] = new Node;
+    }
 
-        for (int i = 0; i < n; i++) {
-            nodes[i] = new Node;
-        }
+    // Parse edge information. Do not add edge (u,v) with weight w if:
+    // 1. v = 0 since we assume 0 is our root node
+    // 2. u == v since this will never be in a minimum directed spanning tree
+    // 3. There is already an edge (u,v) with weight w' and w >= w'
+    for (int i = 0; i < m; i++) {
+        int u, v, w;
+        cin >> u;
+        cin >> v;
+        cin >> w;
 
-        // Parse edge information. Do not add edge (u,v) with weight w if:
-        // 1. v = 0 since we assume 0 is our root node
-        // 2. There is already an edge (u,v) with weight w' and w >= w'
-        for (int i = 0; i < m; i++) {
-            int u, v, w;
-            cin >> u;
-            cin >> v;
-            cin >> w;
-
-            if (v == 0 
-                || u == v
-                || (nodes[v]->edgesComingIn.find(u) != nodes[v]->edgesComingIn.end() 
-                    && w >= nodes[v]->edgesComingIn[u]->w)
-            ) {
-                continue;
-            }
-
-            Edge* edge = new Edge(u, v, w);
-            nodes[u]->edgesGoingOut[v] = edge;
-            nodes[v]->edgesComingIn[u] = edge;
-        }
-
-        // Use DFS to determine if there is a path from the root vertex to all other vertices.
-        // If there is not, there is no spanning directed tree and thus no minimum directed
-        // spanning tree
-        unordered_set<int> seen;
-        DFS(0, seen);
-        if (seen.size() < n) {
-            cout << "Case #" << t + 1 << ": Possums!" << endl;
+        if (v == 0 
+            || u == v
+            || (nodes[v]->edgesComingIn.find(u) != nodes[v]->edgesComingIn.end() 
+                && w >= nodes[v]->edgesComingIn[u]->w)
+        ) {
             continue;
         }
 
-        MDST(n);
-
-        // Every vertex in the MDST (except for the root) has one and only edge going into it.
-        // Sum the weights of all the edges in the MDST by traversing the singular edge going
-        // into each vertex
-        int sum = 0;
-        for (int i = 1; i < n; i++) {
-            Edge* edge = nodes[i]->edgeInMDST;
-            sum += edge->w;
-            // cout << edge->u << " " << edge->v << " " << edge->w << endl;
-        }
-        cout << "Case #" << t + 1 << ": " << sum << endl;
+        Edge* edge = new Edge(u, v, w);
+        nodes[u]->edgesGoingOut[v] = edge;
+        nodes[v]->edgesComingIn[u] = edge;
     }
+
+    // Use DFS to determine if there is a path from the root vertex to all other vertices.
+    // If there is not, there is no spanning directed tree and thus no minimum directed
+    // spanning tree
+    unordered_set<int> seen;
+    DFS(0, seen);
+    if (seen.size() < n) {
+        cout << "No solution!" << endl;
+        return 0;
+    }
+
+    MDST(n);
+
+    // Every vertex in the MDST (except for the root) has one and only edge going into it.
+    // Sum the weights of all the edges in the MDST by traversing the singular edge going
+    // into each vertex
+    int sum = 0;
+    for (int i = 1; i < n; i++) {
+        Edge* edge = nodes[i]->edgeInMDST;
+        sum += edge->w;
+        cout << edge->u << " " << edge->v << " " << edge->w << endl;
+    }
+    cout << "Total sum: " << sum << endl;
 
     return 0;
 }
@@ -194,11 +189,13 @@ void MDST(int n) {
                         edgesToErase.insert(changedEdges[nodes[n - 1]->edgesComingIn[edge.second->u]]);
                     }
                 }
+                // Store old edge and then update edge
                 changedEdges[edge.second] = new Edge(edge.second->u, edge.second->v, edge.second->w);
-                nodes[edge.second->u]->edgesGoingOut.erase(edge.second->v);
-
                 edge.second->v = n - 1;
                 edge.second->w = updatedWeight;
+
+                // Update corresponding edgesGoingOut and edgesComingIn maps for all vertices
+                nodes[edge.second->u]->edgesGoingOut.erase(edge.second->v);
                 nodes[n - 1]->edgesComingIn[edge.second->u] = edge.second;
                 nodes[edge.second->u]->edgesGoingOut[n - 1] = edge.second;
             } else {
@@ -221,10 +218,12 @@ void MDST(int n) {
                         edgesToErase.insert(changedEdges[nodes[n - 1]->edgesGoingOut[edge.second->v]]);
                     }
                 }
+                // Store old edge and then update edge
                 changedEdges[edge.second] = new Edge(edge.second->u, edge.second->v, edge.second->w);
-                nodes[edge.second->v]->edgesComingIn.erase(edge.second->u);
-
                 edge.second->u = n - 1;
+
+                // Update corresponding edgesGoingOut and edgesComingIn maps for all vertices
+                nodes[edge.second->v]->edgesComingIn.erase(edge.second->u);
                 nodes[n - 1]->edgesGoingOut[edge.second->v] = edge.second;
                 nodes[edge.second->v]->edgesComingIn[n - 1] = edge.second;
             } else {
@@ -276,17 +275,21 @@ int findACycle(int vertex, unordered_map<int, Edge*>& vertexToMinEdge, unordered
     
     Edge* edge = vertexToMinEdge[vertex];
     int parentVertex = edge->u;
-    if (parentVertex == 0) { // root vertex
+    if (parentVertex == 0) { 
+        // Parent vertex is root vertex
         return false;
     } 
 
-    if (toRoot.find(parentVertex) != toRoot.end()) { // Parent vertex leads to root
+    if (toRoot.find(parentVertex) != toRoot.end()) { 
+        // Parent vertex leads to root
         toRoot.insert(vertex);
 
         return false;
-    } else if (currentPath.find(parentVertex) == currentPath.end()) { // Haven't seen parent vertex before so recurse
+    } else if (currentPath.find(parentVertex) == currentPath.end()) { 
+        // Haven't seen parent vertex before so recurse
         int foundCycle = findACycle(parentVertex, vertexToMinEdge, toRoot, currentPath, cycleEdges, cycleNodes);
-        if (foundCycle && foundCycle != parentVertex) { // Second check is to see if vertex does not belong in foundCycle
+        if (foundCycle && foundCycle != parentVertex) { 
+            // Second check is to see if vertex does not belong in foundCycle
             cycleEdges.insert(edge);
             cycleNodes.insert(vertex);
 
@@ -296,7 +299,8 @@ int findACycle(int vertex, unordered_map<int, Edge*>& vertexToMinEdge, unordered
 
             return false;
         }
-    } else { // Have seen parent vertex in current path before so this is a cycle
+    } else {
+        // Have seen parent vertex in current path before so this is a cycle
         cycleEdges.insert(edge);
         cycleNodes.insert(vertex);
 
